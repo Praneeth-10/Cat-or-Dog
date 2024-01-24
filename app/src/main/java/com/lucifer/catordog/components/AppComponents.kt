41286d5e6cdbfb5916d238b69b6332ac858dd25a
Praneeth-10 <sp.parasa@gmail.com>
2024-01-23 23:59:16 -0600

    Adding the Top App bar component

--- app/src/main/java/com/lucifer/catordog/components/AppComponents.kt
+++ app/src/main/java/com/lucifer/catordog/components/AppComponents.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Icon
@@ -11,23 +12,35 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.lucifer.catordog.R
 
 @Composable
-fun TopAppBar(strVal : String) {
-    Row (
+fun TopAppBar(strVal: String) {
+    Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp),
+            .padding(10.dp,5.dp),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween,
 
-    ){
-        Text(text = strVal)
-        Icon(imageVector = Icons.Filled.AccountCircle, contentDescription = stringResource(id = R.string.bitmap_description))
+        ) {
+        Text(
+            text = strVal,
+            color = Color.Black,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Medium
+        )
+        Icon(
+            modifier = Modifier.size(50.dp),
+            imageVector = Icons.Filled.AccountCircle,
+            contentDescription = stringResource(id = R.string.bitmap_description)
+        )
 
     }
 }
