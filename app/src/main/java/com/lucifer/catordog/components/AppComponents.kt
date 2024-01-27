6545414f5a7bdde1057a928571216b252e979c7e
Praneeth-10 <sp.parasa@gmail.com>
2024-01-26 23:57:51 -0600

    Adding components for accessing the view model into the application.

--- app/src/main/java/com/lucifer/catordog/components/AppComponents.kt
+++ app/src/main/java/com/lucifer/catordog/components/AppComponents.kt
@@ -1,6 +1,7 @@
 package com.lucifer.catordog.components
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -8,14 +9,20 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.lucifer.catordog.R
@@ -25,7 +32,7 @@ fun TopAppBar(strVal: String) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(10.dp,5.dp),
+            .padding(10.dp, 5.dp),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween,
 
@@ -45,8 +52,36 @@ fun TopAppBar(strVal: String) {
     }
 }
 
+
+@Composable
+fun TextComponent(textValue: String, textSize: TextUnit, textColor: Color = Color.Black) {
+    Text(
+        text = textValue,
+        fontSize = textSize,
+        fontWeight = FontWeight.Light,
+        color = textColor
+    )
+}
+
+@Composable
+fun TextInputComponent(labelName: String, onEventChange: (value: String) -> Unit) {
+    var currentValue by remember {
+        mutableStateOf("")
+    }
+
+    OutlinedTextField(
+        value = currentValue,
+        onValueChange = {
+            onEventChange(it)
+        },
+        )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun TopAppBarPreview() {
-    TopAppBar("Hi, There")
+    Column {
+        TopAppBar("Hi, There")
+        TextComponent(textValue = "hi  Testing", textSize = 24.sp)
+    }
 }
--- app/src/main/java/com/lucifer/catordog/screens/UserInputScreen.kt
+++ app/src/main/java/com/lucifer/catordog/screens/UserInputScreen.kt
@@ -1,19 +1,67 @@
 package com.lucifer.catordog.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.lucifer.catordog.R
+import com.lucifer.catordog.components.TextComponent
+import com.lucifer.catordog.components.TextInputComponent
 import com.lucifer.catordog.components.TopAppBar
+import com.lucifer.catordog.models.UserDataUiEvents
+import com.lucifer.catordog.viewModels.UserInputViewModel
 
 @Composable
-fun UserInputScreen(navController: NavController) {
+fun UserInputScreen(navController: NavController, userInputViewModel: UserInputViewModel = viewModel()) {
     Surface(
         modifier = Modifier
             .fillMaxSize()
     ) {
-        TopAppBar("")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(18.dp)
+        ) {
+            TopAppBar(stringResource(id = R.string.hi_there))
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            TextComponent(
+                textValue = stringResource(id = R.string.lets_learn_about),
+                textSize = 24.sp
+            )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            TextComponent(
+                textValue = stringResource(id = R.string.app_will_prepare),
+                textSize = 20.sp
+            )
+
+            Spacer(modifier = Modifier.size(60.dp))
+
+            TextInputComponent(labelName = ""){
+                userInputViewModel.onEventChanged()
+            }
+        }
     }
+}
+
+@Preview
+@Composable
+fun UserInputScreenPrev() {
+    UserInputScreen(rememberNavController())
 }
