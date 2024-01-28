bd255345004db57daceb0c4e913e6f88e19b62e2
Praneeth-10 <sp.parasa@gmail.com>
2024-01-27 23:51:43 -0600

    Completed the Text field for input with saving data in the view model.

--- app/src/main/java/com/lucifer/catordog/components/AppComponents.kt
+++ app/src/main/java/com/lucifer/catordog/components/AppComponents.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Icon
@@ -19,8 +21,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -69,19 +74,37 @@ fun TextInputComponent(labelName: String, onEventChange: (value: String) -> Unit
         mutableStateOf("")
     }
 
+    val lFocus = LocalFocusManager.current
+
     OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
         value = currentValue,
         onValueChange = {
             onEventChange(it)
+            currentValue = it
         },
-        )
+        label = {
+            Text(text = labelName,
+                style = TextStyle.Default.copy(fontSize = 25.sp))
+        },
+        textStyle = TextStyle.Default.copy(fontSize = 25.sp),
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+        keyboardActions = KeyboardActions{
+            lFocus.clearFocus()
+        }
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun TopAppBarPreview() {
-    Column {
+    Column (
+        modifier = Modifier.padding(10.dp)
+    ){
         TopAppBar("Hi, There")
         TextComponent(textValue = "hi  Testing", textSize = 24.sp)
+        TextInputComponent(labelName = "Entering", onEventChange = {
+
+        })
     }
 }
