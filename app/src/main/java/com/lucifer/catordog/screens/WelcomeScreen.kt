98b0e641aaca4fb69afdd1d14b54f67bc23ac8e7
noobprogramr <sp.parasa@gmail.com>
2024-02-07 00:02:09 -0500

    Adding the welcome Screen components

--- app/src/main/java/com/lucifer/catordog/screens/WelcomeScreen.kt
+++ app/src/main/java/com/lucifer/catordog/screens/WelcomeScreen.kt
@@ -1,10 +1,43 @@
 package com.lucifer.catordog.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import com.lucifer.catordog.R
+import com.lucifer.catordog.components.TopAppBar
+import com.lucifer.catordog.viewModels.UserInputViewModel
 
 @Composable
-fun WelcomeScreen(navHostController: NavHostController) {
-    Text(text = "Welcome Screen")
+fun WelcomeScreen(
+    navHostController: NavHostController,
+    userInputViewModel: UserInputViewModel = viewModel()
+) {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(18.dp)
+        ) {
+            TopAppBar(
+                stringResource(
+                    id = R.string.welcome,
+                    userInputViewModel.uiIs.value.nameEntered + " \\uD83D\\uDE0A"
+                )
+            )
+
+            Text(text = "Welcome Screen")
+        }
+    }
+
 }
