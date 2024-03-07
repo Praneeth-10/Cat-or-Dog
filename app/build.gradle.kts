35bcd94ecb530b3bc97636306f5664284ce24d25
noobprogramr <sp.parasa@gmail.com>
2024-03-06 23:51:17 -0500

    adding a Splash screen to the application

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-    implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
